db47c6fa2d1b3b74983456945a7ed7e687a6bee3
Soraida Henne <sodapoph@gmail.com>
2020-02-17 16:28:38 +0900

    done i hope

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+    @question = params[:question]
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.downcase == 'i am going to work.'
+      @answer = 'Great!'
+    elsif @question.end_with? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
